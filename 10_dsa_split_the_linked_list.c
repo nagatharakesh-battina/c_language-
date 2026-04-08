75204cb281dbaf9576b9164ad6eebe3e4b6ef87d
nagatharakesh-battina <nagatharakeshbattina@gmail.com>
2026-04-08 11:17:42 +0530

    Update 10_dsa_split_the_linked_list.c

--- 10_dsa_split_the_linked_list.c
+++ 10_dsa_split_the_linked_list.c
@@ -1,77 +1,89 @@
-
-
 #include <stdio.h>
-#include<stdlib.h>
-struct Node *head;
- int len=0;
+#include <stdlib.h>
+
 struct Node {
-	int data;
-	struct Node *next;
+    int data;
+    struct Node *next;
 };
 
+struct Node *head = NULL;
+
+// Insert into circular linked list
 void in(int data) {
-	if(head==NULL) {
-		struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    	newNode->data = data;
-    	newNode->next = newNode;
-    	head = newNode;
-	} else {
-		struct Node *temp=head;
-		while(temp->next!=head) {
-			temp=temp->next;
-		}
-		struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-		newNode->data = data;
-		newNode->next = head;
-		temp->next = newNode;
-	}
-	len++;
-}
-void display(struct Node * head) {
-	struct Node *temp=head;
-	printf("\n");
-	while(temp!=NULL) {
-		printf("%d ",temp->data);
-		temp=temp->next;
-	}
-// 	printf("%d\n",temp->data);
-}
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
 
-
-void split(){
-    // printf("Split method\n");
-    struct Node* slow=head;
-    struct Node *fast=head->next;
-    while(fast->next!=head && fast->next->next!=head ){
-        slow=slow->next;
-        fast=fast->next->next;
-        
-    }if(fast->next->next==head){
-        fast->next->next=NULL;
-    }if(fast->next==head){
-        fast->next=NULL;
+    if(head == NULL) {
+        newNode->next = newNode;
+        head = newNode;
+    } else {
+        struct Node *temp = head;
+        while(temp->next != head) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->next = head;
     }
-    fast=slow->next;
-    slow->next=NULL;
-    // printf("calling first time");
-    display(head);
-    display(fast);
-    // printf("end");
 }
 
-int main()
-{
-	int n=1;
-	while(n!=-1) {
-	    scanf("%d",&n);
-		if(n != -1) 
-		    in(n);
+// Display circular list
+void display(struct Node *head) {
+    if(head == NULL) return;
 
-	}
-	printf("\nInsertion completed");
-	split();
+    struct Node *temp = head;
+    do {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    } while(temp != head);
+    printf("\n");
+}
 
+// Split circular linked list
+void split() {
+    struct Node *slow = head;
+    struct Node *fast = head;
 
+    // Find middle using slow & fast
+    while(fast->next != head && fast->next->next != head) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
 
-	return 0;
+    // For even nodes
+    if(fast->next->next == head) {
+        fast = fast->next;
+    }
+
+    struct Node *head1 = head;
+    struct Node *head2 = slow->next;
+
+    // Make first half circular
+    slow->next = head1;
+
+    // Make second half circular
+    fast->next = head2;
+
+    printf("First half:\n");
+    display(head1);
+
+    printf("Second half:\n");
+    display(head2);
+}
+
+int main() {
+    int n;
+
+    while(1) {
+        scanf("%d", &n);
+        if(n == -1) break;
+        in(n);
+    }
+
+    printf("Original List:\n");
+    display(head);
+
+    printf("After Split:\n");
+    split();
+
+    return 0;
 }
