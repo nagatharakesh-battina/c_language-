ab7dfb308cf3a04bfd038dc9d5e2ae131a504906
nagatharakesh-battina <nagatharakeshbattina@gmail.com>
2026-04-19 15:23:10 +0530

    Create Medium_hackerrank_25.c

---
+++ Medium_hackerrank_25.c
@@ -0,0 +1,9 @@
+bool search(int* nums, int numsSize, int target) {
+    for(int i=0;i<numsSize;i++){
+        if(target==nums[i]){
+            return true;
+            break;
+        }
+    }
+    return false;
+}
